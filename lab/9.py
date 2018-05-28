6ef1340c219c2f3c937a01e1fe4013b2c9239b13
Kruszylo <titov@dlit.dp.ua>
2018-05-28 22:03:24 +0200

    homework9a

--- lab/9.py
+++ lab/9.py
@@ -23,19 +23,21 @@ mpl.rcParams['axes.labelsize'] = 12
 
 (x_train, y_train), (x_test, y_test) = fmnist_utils.get_data()
 
-np.random.seed(777)
-ex = x_train[0:40].view(40, 4, 14, 14)
-w = torch.FloatTensor(np.random.uniform(size=(16, 4, 5, 5)))
-b = torch.FloatTensor(np.random.uniform(size=(16,)))
-results['1'] = test_conv2d(ex, w, b, 0, 1)
-
 def test_conv2d(ex, w, b, P, S):
     out_student = conv2d_forward(ex, w, b, P, S)
     out = pytorch_conv2d_foward(ex, w, b, P, S)
     result = np.allclose(out, out_student, atol=1e-2)
     return result
 
+np.random.seed(777)
+ex = x_train[0:40].view(40, 4, 14, 14)
+w = torch.FloatTensor(np.random.uniform(size=(16, 4, 5, 5)))
+b = torch.FloatTensor(np.random.uniform(size=(16,)))
+results['1'] = test_conv2d(ex, w, b, 0, 1)
+
+
 # TODO: Implement this function
+import torch
 def conv2d_forward(input, kernel, bias, padding, stride):
     """
     Params
@@ -54,10 +56,45 @@ def conv2d_forward(input, kernel, bias, padding, stride):
     S = stride #шфг вниз и вправо
     P = padding #ширина паддинга, заполняем нулями
     # TODO: Implement out!
-    w_tensor = torch.zeros([W], dtype=torch.float32)
-    h_tensor = torch.zeros([H+2], dtype=torch.float32)
-    for ind in range(0, N):
-        sample = input[ind]
-        
-    out = np.random.uniform(size=(N, D, (W-F+2*P)/S+1, (H-F+2*P)/S+1))
+    input_with_padding = input
+    if padding>0:
+        zero = torch.zeros([padding], dtype=torch.float32)
+        input_with_padding = torch.zeros([input.shape[0],input.shape[1],W+padding*2,H+padding*2], dtype=torch.float32)
+        for ind in range(0, N):
+            new_sample =  torch.zeros([input.shape[1],W+padding*2,H+padding*2], dtype=torch.float32)
+            for i in range(0,input.shape[1]):#(0..4)
+                new_subsample = torch.zeros([W+padding*2,H+padding*2], dtype=torch.float32)
+                for j in range(0,W):
+                    new_subsample[j] = torch.cat((zero,input[ind][i][j], zero), 0)
+                    new_sample[i][j+padding] = new_subsample[j]
+            input_with_padding[ind] = new_sample
+       
+    
+    w_size = int( (W-F+2*P)/S + 1 )
+    h_size = int( (H-F+2*P)/S + 1 )
+    out=torch.zeros(N, D, w_size, h_size)
+    for sampl in range(N):
+        for filt in range(D):
+            for a in range(w_size):
+                for b in range(h_size):
+                    out[sampl, filt, a, b]=(input_with_padding[sampl,:,(a*S):(a*S+F),(b*S):(b*S+F)]*kernel[filt,:,:,:]).sum()+bias[filt]
+    #out = np.random.uniform(size=(N, D, (W-F+2*P)/S+1, (H-F+2*P)/S+1))
     return out
+def pytorch_conv2d_foward(input, kernel, bias, padding, stride):
+    # Note: You don't have to change this function!
+    # Ugly code to forward input through PyTorch convolution
+    assert kernel.shape[-2] == kernel.shape[-1]
+    kernel_size = kernel.shape[-1]
+    n_filters = kernel.shape[0]
+    n_channels = kernel.shape[1]
+    m = nn.Conv2d(n_channels, n_filters, kernel_size=kernel_size, padding=padding, stride=stride)
+    m.weight.data.copy_(kernel)
+    m.bias.data.copy_(bias)
+    output = m.forward(Variable(input))
+    output = output.data.numpy()
+    return output
+np.random.seed(780)
+ex = x_train[0:40].view(40, 1, 28, 28)
+w = torch.FloatTensor(np.random.uniform(size=(16, 1, 2, 2)))
+b = torch.FloatTensor(np.random.uniform(size=(16,)))
+result = test_conv2d(ex, w, b, 4, 4)
