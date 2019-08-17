d8067830c315984cef3326c738a66c282344a7f5
Maksym <m.titov@student.uj.edu.pl>
2019-08-17 11:21:19 +0200

    all course loaded from local storage

--- lab/11a.py
+++ lab/11a.py
@@ -1,28 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 29 09:17:11 2018
-
-@author: Maxim
-"""
-
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.utils.data as Data
 import torchvision
+
 import time
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import numpy as np
 
-torch.manual_seed(1)    # reproducible
+torch.manual_seed(1)  
 
 # Hyper Parameters
 EPOCH = 10
 BATCH_SIZE = 64
 LR = 0.005         # learning rate
-DOWNLOAD_MNIST = True
+DOWNLOAD_MNIST = False
 N_TEST_IMG = 5
 
 # Mnist digits dataset
@@ -51,7 +45,7 @@ class AutoEncoder(nn.Module):
             nn.Linear(12, D),   # compress to 3 features which can be visualized in plt
         )
         self.decoder = nn.Sequential(
-            nn.Linear(D, 12),
+            nn.Linear(D,12), # Layers in the decoder in autoencoders usually follow a reverse order
             nn.Tanh(),
             nn.Linear(12, 64),
             nn.Tanh(),
@@ -68,51 +62,18 @@ class AutoEncoder(nn.Module):
         return self.encoder(x)
 
     def forward(self, x):
-        encoded = self.encoder(x)
-        decoded = self.decoder(encoded)
-        return encoded, decoded
-    
+        # Encode and decode
+        en = self.encode(x)
+        dec = self.decode(en)
+        return en, dec
 autoencoder = AutoEncoder()
-print(autoencoder)
 
 optimizer = torch.optim.Adam(autoencoder.parameters(), lr=LR)
-loss_func = nn.MSELoss()
+loss_func = nn.MSELoss() # Use MSE loss function
 
 # original data (first row) for viewing
 view_data = Variable(train_data.train_data[:N_TEST_IMG].view(-1, 28*28).type(torch.FloatTensor)/255.)
-EPOCH = 0
-for epoch in range(EPOCH):
-    for step, (x, y) in enumerate(train_loader):
-        b_x = Variable(x.view(-1, 28*28))   # batch x, shape (batch, 28*28)
-        b_y = Variable(x.view(-1, 28*28))   # batch y, shape (batch, 28*28)
-        b_label = Variable(y)               # batch label
 
-        encoded, decoded = autoencoder(b_x)
-
-        loss = loss_func(decoded, b_y)      # mean square error
-        optimizer.zero_grad()               # clear gradients for this training step
-        loss.backward()                     # backpropagation, compute gradients
-        optimizer.step()                    # apply gradients
-
-        if step % 500 == 0 and epoch in [0, 5, EPOCH-1]:
-            print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0])
-
-            # plotting decoded image (second row)
-            _, decoded_data = autoencoder(view_data)
-            
-            # initialize figure
-            f, a = plt.subplots(2, N_TEST_IMG, figsize=(5, 2))
-            
-            for i in range(N_TEST_IMG):
-                a[0][i].imshow(np.reshape(view_data.data.numpy()[i], (28, 28)), cmap='gray'); a[0][i].set_xticks(()); a[0][i].set_yticks(())
-    
-            for i in range(N_TEST_IMG):
-                a[1][i].clear()
-                a[1][i].imshow(np.reshape(decoded_data.data.numpy()[i], (28, 28)), cmap='gray')
-                a[1][i].set_xticks(()); a[1][i].set_yticks(())
-            plt.show();
-            
-#-----------------------------------------------------------
 samples = []
 for step, (x, y) in enumerate(train_loader):
     if len(samples) > 100:
@@ -120,39 +81,5 @@ for step, (x, y) in enumerate(train_loader):
     sample = x.view(-1, 28*28)    
     enc = autoencoder.encode(Variable(sample))
     samples.append(enc.data.numpy())
-    
 sampled_z = np.concatenate(samples, axis=0)
-mean_Z, std_Z = np.mean(sampled_z, axis=0), np.std(sampled_z, axis=0)
-mean_Z
-f, a = plt.subplots(1, N_TEST_IMG, figsize=(5, 2))
-            
-for i in range(N_TEST_IMG):
-    z_sampled = np.random.normal(loc=mean_Z, scale=std_Z).reshape(1, -1)
-    x_decoded = autoencoder.decode(Variable(torch.FloatTensor(z_sampled)))
-    a[i].imshow(np.reshape(x_decoded.data.numpy(), (28, 28)), cmap='gray'); 
-    a[i].set_xticks(()); 
-    a[i].set_yticks(())
-    
-samples = []
-for step, (x, y) in enumerate(train_loader):
-    if len(samples) > 10:
-        break
-        
-    samples.append(1)
-    x_a = x[0:1, :]
-    x_b = x[1:2, :]
-    
-    z_a = autoencoder.encode(Variable(x_a.view(-1, 28*28)))
-    z_b = autoencoder.encode(Variable(x_b.view(-1, 28*28)))
-    
-    f, a = plt.subplots(1, 10, figsize=(10, 3))
-    
-    for i, alpha in enumerate(np.linspace(0, 1, 10)):
-        z_int = z_a + (z_b - z_a) * alpha
-        x_int = autoencoder.decode(z_int)
-        a[i].imshow(np.reshape(x_int.data.numpy(), (28, 28)), cmap='gray'); 
-        a[i].set_xticks(()); 
-        a[i].set_yticks(())
-        
-    
-    plt.show()
+mean_Z, std_Z = np.mean(samples), np.std(samples) # Compute mean and std of the empirical distribution in the latent space
